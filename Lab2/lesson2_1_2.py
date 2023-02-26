fbfea03dba036618f0116fb7f10ed76a27b9d0e4
SvetlanaMaximenko <sv-maximenko@rambler.ru>
2023-02-26 19:45:01 +0300

    Lab2

---
+++ Lab2/lesson2_1_2.py
@@ -0,0 +1,11 @@
+d = [1, 5.0, 9]
+d1 = [1, 5.0, 9]
+
+print(d, d1)
+print(id(d),id(d1))
+
+s = {'Anna', 'Oleg'}
+s1 = {'Anna', 'Oleg'}
+
+print(s, s1)
+print(id(s),id(s1))
---
+++ Lab2/lesson2_5.py
@@ -0,0 +1,6 @@
+friends = {'Egor', 'Liza', 'Vanya', 'Yana'}
+friend = input('Введите имя друга ')
+if friend in friends:
+    print('У меня есть друг с именем', friend)
+else:
+    print('У меня нет друга с именем', friend)
